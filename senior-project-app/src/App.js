ad928f93d7f9ab295659ce30326d8c0e706a7ec6
rbadnin <rbadnin@calpoly.edu>
2023-04-26 11:22:28 -0700

    Cleaned up labels for file upload

--- senior-project-app/src/App.js
+++ senior-project-app/src/App.js
@@ -73,12 +73,6 @@ class App extends Component {
      
       return (
         <div>
-            <h1>
-              GeeksforGeeks
-            </h1>
-            <h3>
-              File Upload using React!
-            </h3>
             <div>
                 <input type="file" onChange={this.onFileChange} />
                 <button onClick={this.onFileUpload}>
